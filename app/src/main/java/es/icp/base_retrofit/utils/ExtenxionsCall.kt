91383addd4cd873649970d5526178b0f715cadc2
icpmoviles <48717898+icpmoviles@users.noreply.github.com>
2022-12-27 13:30:55 +0100

    fix shared flow

--- app/src/main/java/es/icp/base_retrofit/utils/ExtenxionsCall.kt
+++ app/src/main/java/es/icp/base_retrofit/utils/ExtenxionsCall.kt
@@ -3,29 +3,30 @@ package es.icp.base_retrofit.utils
 import android.util.Log
 import es.icp.base_retrofit.communication.NetworkResponse
 import es.icp.base_retrofit.communication.UiState
+import kotlinx.coroutines.flow.MutableStateFlow
 
 const val TAG = "RETROFIT"
 typealias GenericResponse<S> = NetworkResponse<S, Error>
 
-fun NetworkResponse<Any, Error>.executeCall(isSuccessfull: (Boolean?) -> Unit , errorResult: (Int,String) -> Unit?) : Any? {
+fun NetworkResponse<Any, Error>.executeCall(state: MutableStateFlow<UiState>) : Any? {
    return when (val response = this) {
        is NetworkResponse.Success -> {
            Log.w("$TAG SUCCES", response.body.toString())
-           isSuccessfull.invoke(true)
+           state.value = UiState.Success
            response.body
        }
        is NetworkResponse.HttpError ->  {
-           errorResult.invoke(response.code, response.message)
+           state.value = UiState.Error(response.code, response.message)
            Log.w("$TAG HTTP_ERROR", "${response.code} ${response.message}")
            null
        }
        is NetworkResponse.NetworkError -> {
-           errorResult.invoke(response.code ?: -1, response.error.message?: "Se ha producido un error desconocido. Por favor, vuelva a intentarlo pasados unos minutos." )
+           state.value = UiState.Error(response.code ?: -1, response.error.message?: "Se ha producido un error desconocido. Por favor, vuelva a intentarlo pasados unos minutos.")
            Log.w("$TAG NETWORK_ERROR", "${response.code} ${response.error.message}")
            null
        }
        is NetworkResponse.UnknownError -> {
-           errorResult.invoke(-1, response.error?.message ?: "Se ha producido un error desconocido. Por favor, vuelva a intentarlo pasados unos minutos.")
+           state.value = UiState.Error( -1, response.error?.message ?: "Se ha producido un error desconocido. Por favor, vuelva a intentarlo pasados unos minutos.")
            Log.w("$TAG UNKNOWN_ERROR", response.error?.message ?: "Se ha producido un error desconocido. Por favor, vuelva a intentarlo pasados unos minutos.")
            null
        }
