778c44f4c138fc0485a294ced66aecf36fd23d8d
icpmoviles <48717898+icpmoviles@users.noreply.github.com>
2023-01-09 16:26:38 +0100

    pequeños fixes

--- pruebas/src/main/java/es/icp/pruebas/MainActivity.kt
+++ pruebas/src/main/java/es/icp/pruebas/MainActivity.kt
@@ -1,14 +1,13 @@
 package es.icp.pruebas
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import es.icp.base_retrofit.application.OfflineApplication
 import es.icp.base_retrofit.communication.RetrofitBase
 import es.icp.base_retrofit.models.ResponseState
 import es.icp.base_retrofit.utils.checkAccionesPendientes
 import es.icp.base_retrofit.utils.executeCall
-import es.icp.base_retrofit.utils.executeCallWithCheck
 import es.icp.pruebas.mockdata.MockModel
 import es.icp.pruebas.mockdata.MockService
 import kotlinx.coroutines.CoroutineScope
@@ -33,48 +32,30 @@ class MainActivity : AppCompatActivity() {
             Log.w("LISTA", lista.toString())
         }
 
-//        CoroutineScope(Dispatchers.IO).launch {
-//            val response = service.doLogin(
-//                url = "prueba",
-//                mockreques = MockModel(
-//                    "5550",
-//                    Date(),
-//                    "4"
-//                )
-//            ).executeCallWithCheck(
-//                    this@MainActivity.applicationContext,
-//                    true,
-//                    guardarAccion = true
-//                )
-//
-//            when (response) {
-//                is ResponseState.Error -> Log.w("main error", response.toString())
-//                is ResponseState.Ok -> Log.w("main ok", response.data.toString())
-//                is ResponseState.Offline -> Log.w("main ofline", response.toString())
-//            }
-//
-//        }
+        CoroutineScope(Dispatchers.IO)
+            .launch { checkAccionesPendientes(context = this@MainActivity.applicationContext) }
+            .invokeOnCompletion {
+                CoroutineScope(Dispatchers.IO).launch {
+                    val response = service.doLogin(
+                        url = "prueba",
+                        mockreques = MockModel(
+                            "5550",
+                            Date(),
+                            "3"
+                        )
+                    ).executeCall(this@MainActivity.applicationContext, true, guardarAccion = true)
+                    when (response) {
+                        is ResponseState.Error -> Log.w("main error", response.toString())
+                        is ResponseState.Ok -> Log.w("main ok", response.data.toString())
+                        is ResponseState.Offline -> Log.w("main ofline", response.toString())
+                    }
 
-        val job = CoroutineScope(Dispatchers.IO).launch {
-            val response = service.doLogin(
-                url = "prueba",
-                mockreques = MockModel(
-                    "5550",
-                    Date(),
-                    "2"
-                )
-            ).executeCallWithCheck(
-                this@MainActivity.applicationContext,
-                true,
-                guardarAccion = true
-            ) {
-
-                when (it) {
-                    is ResponseState.Error -> Log.w("main error", it.toString())
-                    is ResponseState.Ok -> Log.w("main ok", it.data.toString())
-                    is ResponseState.Offline -> Log.w("main ofline", it.toString())
-                }
             }
+
+
+
+
+
 
         }
 
